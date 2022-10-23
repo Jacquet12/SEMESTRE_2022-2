5045ee33556b95fc4661603081f4cac2b803ddac
Jacquet L <jacquetleme2021@gmail.com>
2022-10-22 19:56:14 -0500

    atualização de novo semestre

---
+++ prog1/pratica.java
@@ -0,0 +1,11 @@
+public class pratica {
+    public static void main(String [] args){
+        System.out.println("- Professora: Andressa sabben\n");
+        System.out.println("- Aluno: Jacquet leme\n");
+        System.out.println("- Objetivo:\n \tO objetivo dessa disciplina é compreender os conceitos fundamentais");
+        System.out.println("\tdo paradigma de programação orientada a objetos e aplicá-los no");
+        System.out.println("\tdesenvolvimento de soluções de software.\n");
+        System.out.println("- Turno: Vespertino\n");
+
+    }
+}
