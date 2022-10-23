3e64504225b3e0ff569d6c6f0a8d2c76b6524b04
Jacquet L <jacquetleme2021@gmail.com>
2022-10-23 11:05:28 -0500

    my comit of the day

---
+++ prog1/pratica4.java
@@ -0,0 +1,12 @@
+public class pratica4 {
+    public static void main(String args[]){
+        int num6 = 6, multi1, num2= 2, multi2;
+
+        for(int i = 1; i <=10; i++){
+            multi1 = i * num6;
+            multi2 = i * num2;
+
+            System.out.println(+i +" x "+ num6 + " = " + multi1  +"\t" +i +" x "+ num2 + " = " + multi2 +"\n");
+        }
+    }
+}
